965c518cc4756deb1d82c4cb0fc2d08b07fce6dd
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-25 13:56:45 -0800

    final touches

--- src/main/java/com/christinacruz/hellohuman/HelloHumanController.java
+++ src/main/java/com/christinacruz/hellohuman/HelloHumanController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloHumanController {
-	
 	@RequestMapping("/")
 	public String index(@RequestParam(value=("name"), required=false)String searchQuery, @RequestParam(value="last_name")String searchQuery2) {
 		if (searchQuery == null) {
